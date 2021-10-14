12a06ad608865f84f24ccc4c4195ae8739a04999
m ungs <ungson001@live.com>
2021-10-14 12:07:22 -0700

    final version

--- actions.js
+++ actions.js
@@ -1,15 +1,61 @@
 // var i = 1
 
-// while(i<101) {
+// while(i < 21) {
 //     console.log(i);
 //     i++;
+
+//     if (i==2) {
+//         console.log('test');
+//     } else {
+        
+//     }
+// } 
+
+
+
+// for(var i = 1; i < 20; i++) {
+//     console.log(i);
+//     // var fizz = i % 3 == 0;
+
+//     if(i==(i % 3 == 0)) {
+//         console.log('test');
+//     } else {
+//     }
 // }
 
-for(var i = 1; i < 20; i++) {
-    console.log(i);
+// var i = 1;
+// var buzz = i % 5 == 0;
+// var fizzBuzz = fizz && buzz;
+// var fizz = i % 3 == 0;
 
-    if(i==2) {
-        console.log('test')
+
+// while(i<41) {
+//     console.log(i);
+//     i++;
+
+    
+// }
+
+// for (i=1; i<21; i++) {
+//     if (i % 3 == 0) {
+//         console.log('Fizz')
+//     } else if (i % 5 == 0) {
+//         console.log('Buzz')
+//     } else if (i % 3 && 5 == 0) {
+//         console.log('FizzBuzz')
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+for (i=1; i<101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 == 0) {
+        console.log('Fizz')
+    } else if (i % 5 == 0) {
+        console.log('Buzz')
     } else {
+        console.log(i)
     }
 }
